dcf487d3facf68bc8784b7a963a1f9d1b3420280
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2024-09-25 20:38:57 +0900

    Fixed incorrect machine type

--- src/main/kotlin/hiiragi283/ragium/common/block/entity/machine/electric/HTAssemblerBlockEntity.kt
+++ src/main/kotlin/hiiragi283/ragium/common/block/entity/machine/electric/HTAssemblerBlockEntity.kt
@@ -5,5 +5,4 @@ import hiiragi283.ragium.common.machine.HTMachineType
 import net.minecraft.block.BlockState
 import net.minecraft.util.math.BlockPos
 
-class HTAssemblerBlockEntity(pos: BlockPos, state: BlockState) :
-    HTSingleMachineBlockEntity(HTMachineType.Single.ALLOY_FURNACE, pos, state)
+class HTAssemblerBlockEntity(pos: BlockPos, state: BlockState) : HTSingleMachineBlockEntity(HTMachineType.Single.ASSEMBLER, pos, state)
