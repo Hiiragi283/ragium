e64e7ad9cc7526f92f4749d9660f27ebd6f4e145
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2024-10-05 12:13:50 +0900

    Fixed incorrect versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -151,6 +151,7 @@ tasks {
     }
 
     processResources {
+        inputs.property("version", project.version)
         filesMatching("fabric.mod.json") {
             expand("version" to project.version)
         }
