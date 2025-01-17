217d243fdc6e259aaa3d74e827b41a4f89e87ebf
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2025-01-17 16:58:55 +0900

    - 機械周りの調整

--- src/main/kotlin/hiiragi283/ragium/common/internal/DefaultMachinePlugin.kt
+++ src/main/kotlin/hiiragi283/ragium/common/internal/DefaultMachinePlugin.kt
@@ -1,6 +1,7 @@
 package hiiragi283.ragium.common.internal
 
 import hiiragi283.ragium.api.RagiumPlugin
+import hiiragi283.ragium.api.block.entity.HTMachineBlockEntity
 import hiiragi283.ragium.api.machine.*
 import hiiragi283.ragium.api.property.HTPropertyHolderBuilder
 import hiiragi283.ragium.common.block.machine.processor.HTDefaultMachineBlockEntity
@@ -9,6 +10,7 @@ import hiiragi283.ragium.common.block.machine.processor.HTLargeMachineBlockEntit
 import hiiragi283.ragium.common.block.machine.processor.HTMultiSmelterBlockEntity
 import hiiragi283.ragium.common.init.RagiumMachineKeys
 import hiiragi283.ragium.common.init.RagiumMultiblockMaps
+import net.minecraft.world.level.block.entity.BlockEntityType
 import java.util.function.BiConsumer
 import java.util.function.Function
 
@@ -31,6 +33,13 @@ object DefaultMachinePlugin : RagiumPlugin {
     @JvmField
     val ELITE_TIERS: List<HTMachineTier> = listOf(HTMachineTier.ELITE, HTMachineTier.ULTIMATE)
 
+    private fun HTPropertyHolderBuilder.putFactory(
+        factory: BlockEntityType.BlockEntitySupplier<out HTMachineBlockEntity>,
+    ): HTPropertyHolderBuilder = put(HTMachinePropertyKeys.MACHINE_FACTORY, HTMachineEntityFactory.of(factory::create))
+
+    private fun HTPropertyHolderBuilder.putFactory(factory: HTMachineEntityFactory): HTPropertyHolderBuilder =
+        put(HTMachinePropertyKeys.MACHINE_FACTORY, factory)
+
     override fun setupMachineProperties(helper: Function<HTMachineKey, HTPropertyHolderBuilder>) {
         // Consumer
 
@@ -40,15 +49,12 @@ object DefaultMachinePlugin : RagiumPlugin {
         RagiumMachineKeys.PROCESSORS
             .map(helper::apply)
             .forEach { builder: HTPropertyHolderBuilder ->
-                builder.put(
-                    HTMachinePropertyKeys.MACHINE_FACTORY,
-                    HTMachineEntityFactory(::HTDefaultMachineBlockEntity),
-                )
+                builder.putFactory(::HTDefaultMachineBlockEntity)
             }
 
         helper
             .apply(RagiumMachineKeys.BLAST_FURNACE)
-            .put(HTMachinePropertyKeys.MACHINE_FACTORY, HTMachineEntityFactory(::HTLargeMachineBlockEntity))
+            .putFactory(::HTLargeMachineBlockEntity)
             .put(HTMachinePropertyKeys.MULTIBLOCK_MAP, RagiumMultiblockMaps.BLAST_FURNACE)
 
         helper
@@ -56,9 +62,17 @@ object DefaultMachinePlugin : RagiumPlugin {
             .put(HTMachinePropertyKeys.VALID_TIERS, ADVANCED_TIERS)
 
         helper
+            .apply(RagiumMachineKeys.CUTTING_MACHINE)
+            .put(HTMachinePropertyKeys.VALID_TIERS, ADVANCED_TIERS)
+
+        helper
             .apply(RagiumMachineKeys.DISTILLATION_TOWER)
-            .put(HTMachinePropertyKeys.MACHINE_FACTORY, HTMachineEntityFactory.of(::HTDistillationTowerBlockEntity))
+            .putFactory(::HTDistillationTowerBlockEntity)
             .put(HTMachinePropertyKeys.MULTIBLOCK_MAP, RagiumMultiblockMaps.DISTILLATION_TOWER)
+            .put(HTMachinePropertyKeys.VALID_TIERS, ADVANCED_TIERS)
+
+        helper
+            .apply(RagiumMachineKeys.GROWTH_CHAMBER)
             .put(HTMachinePropertyKeys.VALID_TIERS, ADVANCED_TIERS)
 
         helper
@@ -67,7 +81,8 @@ object DefaultMachinePlugin : RagiumPlugin {
 
         helper
             .apply(RagiumMachineKeys.MULTI_SMELTER)
-            .put(HTMachinePropertyKeys.MACHINE_FACTORY, HTMachineEntityFactory.of(::HTMultiSmelterBlockEntity))
+            .putFactory(::HTMultiSmelterBlockEntity)
             .put(HTMachinePropertyKeys.MULTIBLOCK_MAP, RagiumMultiblockMaps.MULTI_SMELTER)
+            .put(HTMachinePropertyKeys.VALID_TIERS, ADVANCED_TIERS)
     }
 }
