9c0d257d2d9f1596c4c87f8fa8fb656487455eba
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2025-10-04 23:48:44 +0900

    refactor: unify HTCapabilityResolver into HTCapabilityManager

---
+++ src/main/kotlin/hiiragi283/ragium/common/storage/resolver/HTCapabilityManager.kt
@@ -0,0 +1,31 @@
+package hiiragi283.ragium.common.storage.resolver
+
+import net.minecraft.core.Direction
+import net.neoforged.neoforge.capabilities.BlockCapability
+
+/**
+ * 向きに応じたCapabilityを取得するインターフェース
+ * @param CONTAINER 要素を保持するクラス
+ * @see [mekanism.common.capabilities.resolver.ICapabilityResolver]
+ * @see [mekanism.common.capabilities.resolver.manager.ICapabilityHandlerManager]
+ */
+interface HTCapabilityManager<CONTAINER : Any> {
+    /**
+     * 指定された引数からCapabilityを取得します。
+     * @param T Capabilityのクラス
+     * @param capability Capabilityの種類
+     * @param side アクセスする面
+     * @return 見つからない場合は`null`
+     */
+    fun <T : Any> resolve(capability: BlockCapability<T, Direction?>, side: Direction?): T?
+    
+    /**
+     * このCapabilityが運用可能か判定します。
+     */
+    fun canHandle(): Boolean
+
+    /**
+     * 指定された[side]から[CONTAINER]の一覧を返します。
+     */
+    fun getContainers(side: Direction?): List<CONTAINER>
+}
