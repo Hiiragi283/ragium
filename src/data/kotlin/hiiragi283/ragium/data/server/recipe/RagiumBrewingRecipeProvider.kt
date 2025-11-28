0f6317028aada1e5267a666a16996f73005767f4
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2025-11-29 03:06:53 +0900

    feat: added mush more brewing recipes

--- src/data/kotlin/hiiragi283/ragium/data/server/recipe/RagiumBrewingRecipeProvider.kt
+++ src/data/kotlin/hiiragi283/ragium/data/server/recipe/RagiumBrewingRecipeProvider.kt
@@ -37,68 +37,50 @@ object RagiumBrewingRecipeProvider : HTRecipeProvider.Direct() {
 
     @JvmStatic
     private fun potions() {
-        longBrewing(itemCreator.fromItem(Items.GOLDEN_CARROT), Potions.NIGHT_VISION, Potions.LONG_NIGHT_VISION)
-        longBrewing(itemCreator.fromTagKey(Tags.Items.GEMS_AMETHYST), Potions.INVISIBILITY, Potions.LONG_INVISIBILITY)
-        longAndStrongBrewing(itemCreator.fromItem(Items.RABBIT_FOOT), Potions.LEAPING, Potions.LONG_LEAPING, Potions.STRONG_LEAPING)
-        longBrewing(itemCreator.fromItem(Items.MAGMA_CREAM), Potions.FIRE_RESISTANCE, Potions.LONG_FIRE_RESISTANCE)
-        longAndStrongBrewing(itemCreator.fromItem(Items.SUGAR), Potions.SWIFTNESS, Potions.LONG_SWIFTNESS, Potions.STRONG_SWIFTNESS)
-        longAndStrongBrewing(
-            itemCreator.fromTagKey(Tags.Items.INGOTS_IRON),
-            Potions.SLOWNESS,
-            Potions.LONG_SLOWNESS,
-            Potions.STRONG_SLOWNESS,
-        )
-        longAndStrongBrewing(
-            itemCreator.fromItem(Items.TURTLE_SCUTE),
-            Potions.TURTLE_MASTER,
-            Potions.LONG_TURTLE_MASTER,
-            Potions.STRONG_TURTLE_MASTER,
-        )
-        longBrewing(itemCreator.fromItem(Items.PUFFERFISH), Potions.WATER_BREATHING, Potions.LONG_WATER_BREATHING)
-        strongBrewing(itemCreator.fromItem(Items.GLISTERING_MELON_SLICE), Potions.HEALING, Potions.STRONG_HEALING)
-        strongBrewing(itemCreator.fromItem(Items.FERMENTED_SPIDER_EYE), Potions.HARMING, Potions.STRONG_HARMING)
-        longAndStrongBrewing(itemCreator.fromItem(Items.SPIDER_EYE), Potions.POISON, Potions.LONG_POISON, Potions.STRONG_POISON)
-        longAndStrongBrewing(
-            itemCreator.fromItem(Items.GHAST_TEAR),
-            Potions.REGENERATION,
-            Potions.LONG_REGENERATION,
-            Potions.STRONG_REGENERATION,
-        )
-        longAndStrongBrewing(itemCreator.fromItem(Items.BLAZE_POWDER), Potions.STRENGTH, Potions.LONG_STRENGTH, Potions.STRONG_STRENGTH)
-        longBrewing(itemCreator.fromItem(Items.BONE), Potions.WEAKNESS, Potions.LONG_WEAKNESS)
+        brewing(itemCreator.fromItem(Items.GOLDEN_CARROT), Potions.NIGHT_VISION, Potions.LONG_NIGHT_VISION, null)
+        brewing(itemCreator.fromTagKey(Tags.Items.GEMS_AMETHYST), Potions.INVISIBILITY, Potions.LONG_INVISIBILITY, null)
+        brewing(itemCreator.fromItem(Items.RABBIT_FOOT), Potions.LEAPING, Potions.LONG_LEAPING, Potions.STRONG_LEAPING)
+        brewing(itemCreator.fromItem(Items.MAGMA_CREAM), Potions.FIRE_RESISTANCE, Potions.LONG_FIRE_RESISTANCE, null)
+        brewing(itemCreator.fromItem(Items.SUGAR), Potions.SWIFTNESS, Potions.LONG_SWIFTNESS, Potions.STRONG_SWIFTNESS)
+        brewing(itemCreator.fromTagKey(Tags.Items.INGOTS_IRON), Potions.SLOWNESS, Potions.LONG_SLOWNESS, Potions.STRONG_SLOWNESS)
+        brewing(itemCreator.fromItem(Items.TURTLE_SCUTE), Potions.TURTLE_MASTER, Potions.LONG_TURTLE_MASTER, Potions.STRONG_TURTLE_MASTER)
+        brewing(itemCreator.fromItem(Items.PUFFERFISH), Potions.WATER_BREATHING, Potions.LONG_WATER_BREATHING, null)
+        brewing(itemCreator.fromItem(Items.GLISTERING_MELON_SLICE), Potions.HEALING, null, Potions.STRONG_HEALING)
+        brewing(itemCreator.fromItem(Items.FERMENTED_SPIDER_EYE), Potions.HARMING, null, Potions.STRONG_HARMING)
+        brewing(itemCreator.fromItem(Items.SPIDER_EYE), Potions.POISON, Potions.LONG_POISON, Potions.STRONG_POISON)
+        brewing(itemCreator.fromItem(Items.GHAST_TEAR), Potions.REGENERATION, Potions.LONG_REGENERATION, Potions.STRONG_REGENERATION)
+        brewing(itemCreator.fromItem(Items.BLAZE_POWDER), Potions.STRENGTH, Potions.LONG_STRENGTH, Potions.STRONG_STRENGTH)
+        brewing(itemCreator.fromItem(Items.BONE), Potions.WEAKNESS, Potions.LONG_WEAKNESS, null)
         brewing(itemCreator.fromTagKey(Tags.Items.GEMS_EMERALD), Potions.LUCK)
-        longBrewing(itemCreator.fromItem(Items.PHANTOM_MEMBRANE), Potions.SLOW_FALLING, Potions.LONG_SLOW_FALLING)
+        brewing(itemCreator.fromItem(Items.PHANTOM_MEMBRANE), Potions.SLOW_FALLING, Potions.LONG_SLOW_FALLING, null)
 
-        brewing(
-            itemCreator.fromItem(Items.WIND_CHARGE),
-            Potions.WIND_CHARGED,
-        )
-        brewing(
-            itemCreator.fromItem(Items.COBWEB),
-            Potions.WEAVING,
-        )
-        brewing(
-            itemCreator.fromTagKey(Tags.Items.STORAGE_BLOCKS_SLIME),
-            Potions.OOZING,
-        )
-        brewing(
-            itemCreator.fromTagKey(Tags.Items.STONES),
-            Potions.INFESTED,
-        )
+        brewing(itemCreator.fromItem(Items.WIND_CHARGE), Potions.WIND_CHARGED)
+        brewing(itemCreator.fromItem(Items.COBWEB), Potions.WEAVING)
+        brewing(itemCreator.fromTagKey(Tags.Items.STORAGE_BLOCKS_SLIME), Potions.OOZING)
+        brewing(itemCreator.fromTagKey(Tags.Items.STONES), Potions.INFESTED)
     }
 
     @JvmStatic
     private fun customEffects() {
         // Dig
-        longBrewing(itemCreator.fromItem(Items.GOLDEN_APPLE), MobEffects.DIG_SPEED)
+        mapOf(
+            Items.CAKE to MobEffects.SATURATION,
+            Items.GLOWSTONE to MobEffects.GLOWING,
+            Items.GOLDEN_PICKAXE to MobEffects.DIG_SPEED,
+        ).forEach { (item: Item, effect: Holder<MobEffect>) ->
+            benefitBrewing(itemCreator.fromItem(item), effect)
+        }
 
         mapOf(
+            Items.ECHO_SHARD to MobEffects.DARKNESS,
             Items.POISONOUS_POTATO to MobEffects.CONFUSION,
             Items.ROTTEN_FLESH to MobEffects.HUNGER,
-            Items.WITHER_ROSE to MobEffects.WITHER,
             Items.SHULKER_SHELL to MobEffects.LEVITATION,
+            Items.STONE_PICKAXE to MobEffects.DIG_SLOWDOWN,
+            Items.TINTED_GLASS to MobEffects.BLINDNESS,
+            Items.WITHER_ROSE to MobEffects.WITHER,
         ).forEach { (item: Item, effect: Holder<MobEffect>) ->
-            longAndStrongBrewing(itemCreator.fromItem(item), effect)
+            harmfulBrewing(itemCreator.fromItem(item), effect)
         }
     }
 
@@ -111,57 +93,43 @@ object RagiumBrewingRecipeProvider : HTRecipeProvider.Direct() {
     )
 
     @JvmStatic
-    private fun brewing(
-        right: HTItemIngredient,
-        potion: Holder<Potion>,
-        left: HTItemIngredient = itemCreator.fromTagKey(Tags.Items.CROPS_NETHER_WART),
-    ) {
+    private fun brewing(right: HTItemIngredient, potion: Holder<Potion>) {
         HTCombineRecipeBuilder
             .brewing(
-                left,
+                itemCreator.fromTagKey(Tags.Items.CROPS_NETHER_WART),
                 right,
                 potion,
             ).save(output)
     }
 
     @JvmStatic
-    private fun longBrewing(right: HTItemIngredient, base: Holder<Potion>, long: Holder<Potion>) {
-        // Base
-        brewing(right, base)
-        // Long
-        brewing(
-            itemCreator.fromTagKey(CommonMaterialPrefixes.DUST, VanillaMaterialKeys.REDSTONE),
-            long,
-            dropIngredient(base),
-        )
-    }
-
-    @JvmStatic
-    private fun strongBrewing(right: HTItemIngredient, base: Holder<Potion>, strong: Holder<Potion>) {
-        // Base
-        brewing(right, base)
-        // Strong
-        brewing(
-            itemCreator.fromTagKey(CommonMaterialPrefixes.DUST, VanillaMaterialKeys.GLOWSTONE),
-            strong,
-            dropIngredient(base),
-        )
-    }
-
-    @JvmStatic
-    private fun longAndStrongBrewing(
+    private fun brewing(
         right: HTItemIngredient,
         base: Holder<Potion>,
-        long: Holder<Potion>,
-        strong: Holder<Potion>,
+        long: Holder<Potion>?,
+        strong: Holder<Potion>?,
     ) {
-        longBrewing(right, base, long)
+        // Base
+        brewing(right, base)
+        val drop: HTItemIngredient = dropIngredient(base)
+        // Long
+        if (long != null) {
+            HTCombineRecipeBuilder
+                .brewing(
+                    itemCreator.fromTagKey(CommonMaterialPrefixes.DUST, VanillaMaterialKeys.REDSTONE),
+                    drop,
+                    long,
+                ).save(output)
+        }
         // Strong
-        brewing(
-            itemCreator.fromTagKey(CommonMaterialPrefixes.DUST, VanillaMaterialKeys.GLOWSTONE),
-            strong,
-            dropIngredient(base),
-        )
+        if (strong != null) {
+            HTCombineRecipeBuilder
+                .brewing(
+                    itemCreator.fromTagKey(CommonMaterialPrefixes.DUST, VanillaMaterialKeys.GLOWSTONE),
+                    drop,
+                    strong,
+                ).save(output)
+        }
     }
 
     // MobEffectInstance
@@ -183,21 +151,24 @@ object RagiumBrewingRecipeProvider : HTRecipeProvider.Direct() {
         )
 
     @JvmStatic
-    private fun longBrewing(right: HTItemIngredient, effect: Holder<MobEffect>) {
-        val instance = MobEffectInstance(effect, 3600)
-        val drop: HTItemIngredient = dropIngredient(instance)
-        // Base
-        brewing(right) { add(instance) }.save(output)
-        // Long
-        brewing(
-            itemCreator.fromTagKey(CommonMaterialPrefixes.DUST, VanillaMaterialKeys.REDSTONE),
-            drop,
-        ) { add(MobEffectInstance(effect, 9600)) }.savePrefixed(output, "long_")
+    private fun benefitBrewing(right: HTItemIngredient, effect: Holder<MobEffect>) {
+        longAndStrongBrewing(right, effect, 3600, 9600, 1800)
     }
 
     @JvmStatic
-    private fun longAndStrongBrewing(right: HTItemIngredient, effect: Holder<MobEffect>) {
-        val instance = MobEffectInstance(effect, 900)
+    private fun harmfulBrewing(right: HTItemIngredient, effect: Holder<MobEffect>) {
+        longAndStrongBrewing(right, effect, 900, 1800, 432)
+    }
+
+    @JvmStatic
+    private fun longAndStrongBrewing(
+        right: HTItemIngredient,
+        effect: Holder<MobEffect>,
+        baseTime: Int,
+        longTime: Int,
+        strongTime: Int,
+    ) {
+        val instance = MobEffectInstance(effect, baseTime)
         val drop: HTItemIngredient = dropIngredient(instance)
         // Base
         brewing(right) { add(instance) }.save(output)
@@ -205,11 +176,11 @@ object RagiumBrewingRecipeProvider : HTRecipeProvider.Direct() {
         brewing(
             itemCreator.fromTagKey(CommonMaterialPrefixes.DUST, VanillaMaterialKeys.REDSTONE),
             drop,
-        ) { add(MobEffectInstance(effect, 1800)) }.savePrefixed(output, "long_")
+        ) { add(MobEffectInstance(effect, longTime)) }.savePrefixed(output, "long_")
         // Strong
         brewing(
             itemCreator.fromTagKey(CommonMaterialPrefixes.DUST, VanillaMaterialKeys.GLOWSTONE),
             drop,
-        ) { add(MobEffectInstance(effect, 432, 1)) }.savePrefixed(output, "strong_")
+        ) { add(MobEffectInstance(effect, strongTime, 1)) }.savePrefixed(output, "strong_")
     }
 }
