cde3f65b638a315aa41d0ab0d689f9be3ce6ea75
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2026-08-28 22:34:55 +0900

    feat: 仮の機械モデルとテクスチャを用意

--- src/data/kotlin/hiiragi283/ragium/data/model/RagiumModelProvider.kt
+++ src/data/kotlin/hiiragi283/ragium/data/model/RagiumModelProvider.kt
@@ -1,14 +1,25 @@
 package hiiragi283.ragium.data.model
 
+import hiiragi283.lib.HTConstants
 import hiiragi283.lib.data.model.HTModelProvider
+import hiiragi283.lib.data.model.createBlock
 import hiiragi283.lib.registry.HTFluidContent
+import hiiragi283.lib.resource.SupplierWithId
+import hiiragi283.lib.resource.modifyPath
 import hiiragi283.ragium.api.RagiumAPI
+import hiiragi283.ragium.api.tag.HTMachineType
 import hiiragi283.ragium.common.block.RagiumBlocks
 import hiiragi283.ragium.common.fluid.RagiumFluids
 import hiiragi283.ragium.common.item.RagiumItems
 import net.minecraft.client.data.models.BlockModelGenerators
 import net.minecraft.client.data.models.ItemModelGenerators
+import net.minecraft.client.data.models.blockstates.MultiVariantGenerator
+import net.minecraft.client.data.models.model.ModelTemplates
+import net.minecraft.client.data.models.model.TextureMapping
+import net.minecraft.client.data.models.model.TextureSlot
+import net.minecraft.client.resources.model.sprite.Material
 import net.minecraft.data.PackOutput
+import net.minecraft.world.level.block.Block
 
 class RagiumModelProvider(output: PackOutput) : HTModelProvider(output, RagiumAPI.MOD_ID) {
     override fun registerModels(blockModels: BlockModelGenerators, itemModels: ItemModelGenerators) {
@@ -47,7 +58,27 @@ class RagiumModelProvider(output: PackOutput) : HTModelProvider(output, RagiumAP
         RagiumBlocks.MATERIAL_BLOCKS.values.forEach { generators.createTrivialCube(it.get()) }
 
         // Machine
-        RagiumBlocks.MACHINES.values.forEach { generators.createNonTemplateModelBlock(it.get()) }
+        for ((machineType: HTMachineType, block: SupplierWithId<Block>) in RagiumBlocks.MACHINES.flatEntries) {
+            val casing = Material(RagiumAPI.id(HTConstants.BLOCK, "machine", "casing", machineType.materialName))
+            generators.blockStateOutput.accept(
+                MultiVariantGenerator.dispatch(
+                    block.get(),
+                    BlockModelGenerators.plainVariant(
+                        ModelTemplates.CUBE_ORIENTABLE.createBlock(
+                            block,
+                            TextureMapping()
+                                .put(TextureSlot.TOP, casing)
+                                .put(TextureSlot.SIDE, casing)
+                                .put(
+                                    TextureSlot.FRONT,
+                                    Material(block.getId().modifyPath { "${HTConstants.BLOCK}/mahcine/${it}_front" }),
+                                ),
+                            generators.modelOutput,
+                        ),
+                    ),
+                ).with(BlockModelGenerators.ROTATION_HORIZONTAL_FACING),
+            )
+        }
     }
 
     private fun registerItemModels(generators: ItemModelGenerators) {
