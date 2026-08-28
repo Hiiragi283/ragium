edaeff27c38fa6d177d0714135db45ad9db23c8d
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2026-08-28 21:18:40 +0900

    feat!: 連携向けのために DeferredHolder を強制的にエンコードするようにしてみた

---
+++ src/main/java/hiiragi283/ragium/mixin/RegistryFixedCodecMixin.java
@@ -0,0 +1,28 @@
+package hiiragi283.ragium.mixin;
+
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.DynamicOps;
+import net.minecraft.core.Holder;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.RegistryFixedCodec;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(RegistryFixedCodec.class)
+public abstract class RegistryFixedCodecMixin<E> {
+    @Inject(
+            method =
+                    "encode(Lnet/minecraft/core/Holder;Lcom/mojang/serialization/DynamicOps;Ljava/lang/Object;)Lcom/mojang/serialization/DataResult;",
+            at = @At("HEAD"),
+            cancellable = true)
+    private <T> void ragium$encode(
+            Holder<E> input, DynamicOps<T> ops, T prefix, CallbackInfoReturnable<DataResult<T>> cir) {
+        // 入力がDeferredHolderかつ値が紐づかない場合，IDから強制的にエンコードさせる
+        if (input instanceof DeferredHolder<E, ?> holder && !holder.isBound()) {
+            cir.setReturnValue(Identifier.CODEC.encode(holder.getId(), ops, prefix));
+        }
+    }
+}
---
+++ src/main/java/hiiragi283/ragium/mixin/RegistryMixin.java
@@ -0,0 +1,36 @@
+package hiiragi283.ragium.mixin;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.DynamicOps;
+import com.mojang.serialization.Encoder;
+import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.Identifier;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Registry.class)
+public interface RegistryMixin<E> {
+    @Inject(method = "holderByNameCodec", at = @At("RETURN"), cancellable = true)
+    private void ragium$holderByNameCodec(CallbackInfoReturnable<Codec<Holder<E>>> cir) {
+        Codec<Holder<E>> original = cir.getReturnValue();
+        cir.setReturnValue(Codec.of(
+                new Encoder<>() {
+                    @Override
+                    public <T> DataResult<T> encode(Holder<E> input, DynamicOps<T> ops, T prefix) {
+                        // 入力がDeferredHolderかつ値が紐づかない場合，IDから強制的にエンコードさせる
+                        if (input instanceof DeferredHolder<E, ?> holder && !holder.isBound()) {
+                            return Identifier.CODEC.encode(holder.getId(), ops, prefix);
+                        } else {
+                            return original.encode(input, ops, prefix);
+                        }
+                    }
+                },
+                original,
+                original.toString()));
+    }
+}
