91afd3271e5457770cea445f59e59983ae73bd96
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2026-09-01 19:28:00 +0900

    build: java formatterをpalantirからgoogle java formatに変更

--- src/main/java/hiiragi283/ragium/mixin/RegistryFixedCodecMixin.java
+++ src/main/java/hiiragi283/ragium/mixin/RegistryFixedCodecMixin.java
@@ -2,11 +2,13 @@ package hiiragi283.ragium.mixin;
 
 import com.mojang.serialization.DataResult;
 import com.mojang.serialization.DynamicOps;
+
 import net.minecraft.core.Holder;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.RegistryFixedCodec;
 import net.neoforged.neoforge.data.loading.DatagenModLoader;
 import net.neoforged.neoforge.registries.DeferredHolder;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -20,7 +22,10 @@ public abstract class RegistryFixedCodecMixin<E> {
             at = @At("HEAD"),
             cancellable = true)
     private <T> void ragium$encode(
-            Holder<E> input, DynamicOps<T> ops, T prefix, CallbackInfoReturnable<DataResult<T>> cir) {
+            Holder<E> input,
+            DynamicOps<T> ops,
+            T prefix,
+            CallbackInfoReturnable<DataResult<T>> cir) {
         if (!DatagenModLoader.isRunningDataGen()) return;
         // 入力がDeferredHolderかつ値が紐づかない場合，IDから強制的にエンコードさせる
         if (input instanceof DeferredHolder<E, ?> holder && !holder.isBound()) {
