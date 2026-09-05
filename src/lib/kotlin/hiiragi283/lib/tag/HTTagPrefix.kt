297461e2e88fab35e8fa7d75ee47281ababb6a6a
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2026-09-05 16:59:15 +0900

    feat: 機械のタグの名前空間を"c"から"ragium"に変更

--- src/lib/kotlin/hiiragi283/lib/tag/HTTagPrefix.kt
+++ src/lib/kotlin/hiiragi283/lib/tag/HTTagPrefix.kt
@@ -1,6 +1,7 @@
 package hiiragi283.lib.tag
 
 import hiiragi283.lib.HTConstants
+import net.minecraft.resources.Identifier
 import net.minecraft.tags.TagKey
 import net.minecraft.world.item.Item
 import net.minecraft.world.level.block.Block
@@ -10,18 +11,29 @@ import net.minecraft.world.level.block.Block
  * @author Hiiragi Tsubasa
  * @since 26.1.0
  */
-class HTTagPrefix(val rawCommonTag: BlockItemTag, private val tagPattern: String) {
+class HTTagPrefix(
+    val rawCommonTag: BlockItemTag,
+    private val tagPattern: String,
+    private val childNamespace: String = HTConstants.COMMON
+) {
+    constructor(
+        commonTagId: Identifier,
+        tagPattern: String,
+        childNamespace: String = HTConstants.COMMON
+    ) : this(BlockItemTag(commonTagId), tagPattern, childNamespace)
+
     constructor(
         commonTagId: String,
-        tagPattern: String
-    ) : this(BlockItemTag(HTConstants.COMMON, commonTagId), tagPattern)
+        tagPattern: String,
+        childNamespace: String = HTConstants.COMMON
+    ) : this(BlockItemTag(HTConstants.COMMON, commonTagId), tagPattern, childNamespace)
 
     /**
      * 素材の共通タグを生成します。
      * @param material 素材の種類
      */
     fun materialTag(material: HTMaterialLike): BlockItemTag =
-        BlockItemTag(HTConstants.COMMON, tagPattern.replace("%s", material.materialName))
+        BlockItemTag(childNamespace, tagPattern.replace("%s", material.materialName))
 
     /**
      * ブロックの素材の共通タグを生成します。
